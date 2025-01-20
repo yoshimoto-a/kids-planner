ff3cee2a728a378c0b8f144d39268d4a00e07c41
rs-akane <rs.akane.y@gmail.com>
2025-01-20 14:27:38 +0900

    コンテンツ追加

--- src/app/_components/Title.tsx
+++ src/app/_components/Title.tsx
@@ -3,5 +3,11 @@ interface Props {
   children: ReactNode;
 }
 export const Title: React.FC<Props> = ({ children }) => {
-  return <h2 className="text-2xl text-center">{children}</h2>;
+  return (
+    <h2
+      className={`text-center relative text-2xl font-bold before:absolute before:left-16 before:top-3 before:h-[3px] before:w-[30px] before:translate-y-1/2 before:rotate-[60deg] before:rounded-sm before:bg-accentBeige  before:content-[''] after:absolute after:right-16 after:top-3 after:h-[3px] after:w-[30px] after:translate-y-1/2 after:rotate-[-60deg] after:rounded-sm after:bg-accentBeige after:content-['']`}
+    >
+      {children}
+    </h2>
+  );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,114 @@
 "use client";
 import { LoginButton } from "./_components/LoginButton";
 import { WorryItem } from "./_components/WorryItem";
-import { Title } from "./_components/Title";
-import Image from "next/image";
 import { Toaster } from "react-hot-toast";
+import { Section } from "./_components/(lp)/Section";
+import { Step } from "./_components/(lp)/Step";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Home() {
   return (
     <div className="max-w-[480px] mx-auto pt-[60px] px-2">
       <Toaster position="top-center" />
-      <div className="flex flex-col justify-center items-center gap-12 pt-10">
+      <div className="flex flex-col justify-center items-center gap-12 pt-10 pb-12">
         <div className="text-lg text-center">
           子供の長期休みをもっと楽しく！
           <br />
           宿題を自分からやりたくなるアプリ
         </div>
-        <div className="text-4xl">kids planner</div>
+        <div className="text-4xl font-bold">kids planner</div>
         <LoginButton>Googleアカウントではじめる</LoginButton>
-        <div className="flex flex-col items-center gap-4">
-          <Title>長期休みの悩みあるある</Title>
-          <ul className="text-sm flex flex-col gap-2">
-            <WorryItem text="宿題やった？って聞きたくない" />
-            <WorryItem text="宿題が多くて提出日を忘れる(夏)" />
-            <WorryItem text="長期休みも勉強を習慣化してほしい" />
-            <WorryItem text="宿題リストを手作りして貼っている" />
-          </ul>
-          <p>この悩み、kids plannerで解決します！</p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Title>保護者用画面</Title>
+      </div>
+      <div className="flex flex-col items-center gap-6 pb-20">
+        <h2 className="text-2xl">長期休みの悩みあるある</h2>
+        <ul className="text-sm flex flex-col gap-2">
+          <WorryItem text="宿題やった？って聞きたくない" />
+          <WorryItem text="宿題が多くて提出日を忘れる(夏)" />
+          <WorryItem text="長期休みも勉強を習慣化してほしい" />
+          <WorryItem text="宿題リストを手作りして貼っている" />
+        </ul>
+        <p className="font-bold text-xl">
+          この悩み、kids plannerで解決します！
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center gap-2 pb-6">
+        <FontAwesomeIcon
+          className="text-2xl text-accentBeige"
+          icon={faPencil}
+        />
+        <h2 className="text-2xl font-bold">登録STEP</h2>
+      </div>
+      <div className="flex flex-col items-center gap-4 pb-20">
+        <Step
+          title="お子さまの設定"
+          content="googleアカウントでログイン時に1人のお子さまが設定されます。"
+          number="1"
+        />
+        <Step
+          title="長期休暇の設定"
+          content="お子さま毎に登録し、今現在の休暇を有効化してください。"
+          number="2"
+        />
+        <Step
+          title="宿題の登録"
+          content="お子さまごとに管理したい宿題を登録してください。"
+          number="3"
+        />
+      </div>
+
+      <div className="flex flex-col gap-8">
+        <Section title="ダッシュボード" imageSrc="/toppage/dashboard.jpg">
           <p className="text-center">
-            子供は何人でも登録可能
+            休暇の残日数や宿題の進捗が
             <br />
-            もちろん子供毎の宿題進捗管理
+            一目でわかるダッシュボード
           </p>
-          <Image
-            src={"/toppage/admin.png"}
-            alt="保護者画面"
-            width={400}
-            height={800}
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <Title>子供操作用の画面</Title>
+        </Section>
+
+        <Section title="管理画面" imageSrc="/toppage/setting.jpg">
           <p className="text-center">
-            シール貼りたい、トイレに貼りたい
+            お子さまの登録は人数制限なし
             <br />
-            紙媒体へのニーズに合わせた印刷機能
+            長期休暇毎に宿題管理ができる
           </p>
-          <Image
-            src={"/toppage/child.png"}
-            alt="子供操作画面"
-            width={400}
-            height={800}
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <h2 className="text-2xl text-center">「できた」押したくなるボタン</h2>
+        </Section>
+        <Section title="お子さま用画面" imageSrc="/toppage/child.jpg">
+          <div className="flex flex-col gap-2">
+            <p className="text-center">
+              「できた」ボタンを押せば
+              <br />
+              顔文字が散って祝福する楽しい演出
+            </p>
+          </div>
+        </Section>
+        <Section title="宿題の登録" imageSrc="/toppage/homeworks.jpg">
           <p className="text-center">
-            宿題が一つできるごとに押せるボタン
+            宿題登録や進捗の管理ができる
             <br />
-            絵文字が飛び散り、喜ぶこと間違いなし
+            保護者用画面
           </p>
-          <Image
-            src={"/toppage/done.png"}
-            alt="done"
-            width={400}
-            height={800}
-          />
-        </div>
-        <div className="flex flex-col items-center gap-5 pb-5">
-          <h3>親子で楽しく宿題管理してみませんか？</h3>
-          <LoginButton>使ってみる</LoginButton>
-        </div>
+        </Section>
+
+        <Section title="カレンダー" imageSrc="/toppage/calendar.jpg">
+          <p className="text-center">
+            お子さま別に色が設定できるカレンダー
+            <br />
+            宿題の一覧表示も可能
+          </p>
+        </Section>
+
+        <Section title="印刷機能" imageSrc="/toppage/print.jpg">
+          <p className="text-center">
+            印刷画面のプレビュー表示
+            <br />
+            この画面からもカンタン印刷
+          </p>
+        </Section>
+      </div>
+      <div className="flex flex-col items-center gap-5 pb-20">
+        <h3>親子で楽しく宿題管理してみませんか？</h3>
+        <LoginButton>使ってみる</LoginButton>
       </div>
     </div>
   );
